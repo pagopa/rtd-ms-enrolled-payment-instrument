475682f0adb3f245c7160e4c77735799f67be302
andrea.petreti <andrea.petreti@pagopa.it>
2022-10-12 09:35:49 +0200

    improved domain test coverage

--- src/test/java/it/gov/pagopa/rtd/ms/enrolledpaymentinstrument/domain/PaymentInstrumentTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/enrolledpaymentinstrument/domain/PaymentInstrumentTest.java
@@ -4,11 +4,16 @@ import it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.TestUtils;
 import it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.domain.entities.EnrolledPaymentInstrument;
 import it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.domain.entities.SourceApp;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtensionContext;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.ArgumentsProvider;
+import org.junit.jupiter.params.provider.ArgumentsSource;
 
 import java.util.Set;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class PaymentInstrumentTest {
 
@@ -101,12 +106,40 @@ class PaymentInstrumentTest {
   @Test
   void whenEnrollAppToRevokedInstrumentThenItKeepRevokedState() {
     final var paymentInstrument = EnrolledPaymentInstrument.createUnEnrolledInstrument(
-            TestUtils.generateRandomHashPan(),
-            "",
-            ""
+            TestUtils.generateRandomHashPan(), "", ""
     );
     paymentInstrument.revokeInstrument();
     paymentInstrument.enableApp(SourceApp.ID_PAY);
     assertTrue(paymentInstrument.isRevoked());
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(RandomPaymentInstrumentProvider.class)
+  void whenUpdateParInfoThenInstrumentDoesntChangeState(EnrolledPaymentInstrument paymentInstrument) {
+    final var currentState = paymentInstrument.getState();
+    paymentInstrument.associatePar("par");
+    assertEquals(currentState, paymentInstrument.getState());
+  }
+
+  @ParameterizedTest
+  @ArgumentsSource(RandomPaymentInstrumentProvider.class)
+  void whenUpdateChildHashPanThenInstrumentDoesntChangeState(EnrolledPaymentInstrument paymentInstrument) {
+    final var currentState = paymentInstrument.getState();
+    paymentInstrument.addHashPanChild(TestUtils.generateRandomHashPan());
+    assertEquals(currentState, paymentInstrument.getState());
+  }
+
+  static class RandomPaymentInstrumentProvider implements ArgumentsProvider {
+
+    @Override
+    public Stream<? extends Arguments> provideArguments(ExtensionContext extensionContext) throws Exception {
+      final var revokedCard = EnrolledPaymentInstrument.create(TestUtils.generateRandomHashPan(), Set.of(SourceApp.FA), "", "");
+      revokedCard.revokeInstrument();
+      return Stream.of(
+              Arguments.of(EnrolledPaymentInstrument.createUnEnrolledInstrument(TestUtils.generateRandomHashPan(), "", "")),
+              Arguments.of(EnrolledPaymentInstrument.create(TestUtils.generateRandomHashPan(), Set.of(SourceApp.FA), "", "")),
+              Arguments.of(revokedCard)
+      );
+    }
   }
 }
