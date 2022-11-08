accd81816e5a690f7bcf5537fa867d588231ddbf
andrea.petreti <andrea.petreti@pagopa.it>
2022-11-08 15:25:10 +0100

    more elegant router functionality

--- src/main/java/it/gov/pagopa/rtd/ms/enrolledpaymentinstrument/ports/event/routes/PaymentInstrumentEventRouter.java
+++ src/main/java/it/gov/pagopa/rtd/ms/enrolledpaymentinstrument/ports/event/routes/PaymentInstrumentEventRouter.java
@@ -3,14 +3,14 @@ package it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.ports.event.routes;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.common.CloudEvent;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.function.context.MessageRoutingCallback;
 import org.springframework.messaging.Message;
 
-import java.lang.reflect.Type;
-import java.util.*;
-import java.util.function.Predicate;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Optional;
+import java.util.UnknownFormatConversionException;
 
 /**
  * Cause rtd-enrolled-pi kafka queue is used to publish event from specific application
@@ -37,26 +37,25 @@ public class PaymentInstrumentEventRouter implements MessageRoutingCallback {
   @Override
   public FunctionRoutingResult routingResult(Message<?> message) {
     try {
-      final var rawPayload = message.getPayload();
-      Optional<Map<String, Object>> payload;
-
-      if (rawPayload instanceof String) {
-        payload = Optional.ofNullable(objectMapper.readValue(rawPayload.toString(), rawTypeReference));
-      } else if (rawPayload instanceof byte[]) {
-        payload = Optional.ofNullable(objectMapper.readValue(new String((byte[]) rawPayload), rawTypeReference));
-      } else {
-        payload = Optional.empty();
-      }
-
-      return payload
+      return StringMessageUtils.convertPayloadToString(message)
+              .flatMap(this::parseToJson)
               .flatMap(it -> Optional.ofNullable(it.get("type").toString()))
               .flatMap(it -> Optional.ofNullable(routingMap.get(it)))
               .map(FunctionRoutingResult::new)
-              .orElseThrow(() -> new UnknownFormatConversionException("Unknown format " + rawPayload.getClass()));
+              .orElseThrow(() -> new UnknownFormatConversionException("Unknown format " + message.getPayload().getClass()));
 
-    } catch (JsonProcessingException | UnknownFormatConversionException exception) {
+    } catch (UnknownFormatConversionException exception) {
       log.warn("Unknown event or fail during parse as json", exception);
       throw new UnknownFormatConversionException(exception.getMessage());
+    }
+  }
+
+  private Optional<Map<String, Object>> parseToJson(String payload) {
+    try {
+      return Optional.ofNullable(objectMapper.readValue(payload, rawTypeReference));
+    } catch (JsonProcessingException e) {
+      log.warn("Fail during parse as json", e);
+      return Optional.empty();
     }
   }
 }
