aebbd220bda114b57f9030c4e0fdbdad938d28a1
petretiandrea <andrea.petreti@pagopa.it>
2023-01-26 13:59:37 +0100

    feat: Add producer partition count (#76) (#77)
    * add producer partition key
    
    * fix env variable partition count

--- src/test/java/it/gov/pagopa/rtd/ms/enrolledpaymentinstrument/infrastructure/kafka/ack/KafkaEnrollAckServiceTest.java
+++ src/test/java/it/gov/pagopa/rtd/ms/enrolledpaymentinstrument/infrastructure/kafka/ack/KafkaEnrollAckServiceTest.java
@@ -9,6 +9,7 @@ import it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.configs.KafkaTestConfigura
 import it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.domain.entities.SourceApp;
 import it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.infrastructure.kafka.CorrelationIdService;
 import lombok.SneakyThrows;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -29,15 +30,18 @@ import org.springframework.test.context.ActiveProfiles;
 import java.time.Duration;
 import java.util.Date;
 import java.util.List;
+import java.util.NoSuchElementException;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
 
 @SpringBootTest
 @ActiveProfiles("kafka-test")
-@EmbeddedKafka(bootstrapServersProperty = "spring.embedded.kafka.brokers")
+@EmbeddedKafka(bootstrapServersProperty = "spring.embedded.kafka.brokers", partitions = 3)
 @ImportAutoConfiguration(ValidationAutoConfiguration.class)
 @Import({KafkaTestConfiguration.class})
 @EnableAutoConfiguration(exclude = {TestSupportBinderAutoConfiguration.class, EmbeddedMongoAutoConfiguration.class})
@@ -91,6 +95,20 @@ class KafkaEnrollAckServiceTest {
               .matches(it -> Objects.equals(it.getData().getTimestamp(), ackTimestamp))
               .matches(it -> Objects.equals(it.getData().getApplication(), SourceApp.ID_PAY))
               .matches(it -> Objects.equals(it.getCorrelationId(), "1234"));
+    });
+  }
+
+  @Test
+  void whenPublishApplicationInstrumentEventThenShouldBeProducedOnDifferentPartitions() {
+    final var hashPans = IntStream.range(0, 10).mapToObj(i -> TestUtils.generateRandomHashPan());
+
+    hashPans.forEach(it -> kafkaEnrollAckService.confirmEnroll(SourceApp.ID_PAY, it, new Date()));
+
+    await().ignoreException(NoSuchElementException.class).atMost(Duration.ofSeconds(10)).untilAsserted(() -> {
+      final var records = testConsumer.getConsumerRecords();
+      final var partitions = records.stream().collect(Collectors.groupingBy(ConsumerRecord::partition));
+      assertThat(records).hasSize(10);
+      assertThat(partitions).hasSize(3);
     });
   }
 }
