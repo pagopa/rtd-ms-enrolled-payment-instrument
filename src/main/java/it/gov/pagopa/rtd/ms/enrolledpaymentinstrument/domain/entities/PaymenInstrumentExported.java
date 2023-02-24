dd3f4d58a060661da14793cf8f75b02c5a18b74d
andrea.petreti <andrea.petreti@pagopa.it>
2023-02-24 10:11:39 +0100

    remove typo class

--- src/main/java/it/gov/pagopa/rtd/ms/enrolledpaymentinstrument/domain/entities/PaymenInstrumentExported.java
+++
@@ -1,9 +0,0 @@
-package it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.domain.entities;
-
-import it.gov.pagopa.rtd.ms.enrolledpaymentinstrument.common.DomainEvent;
-import lombok.Data;
-
-@Data
-public class PaymenInstrumentExported implements DomainEvent {
-  private final String hashPan;
-}
